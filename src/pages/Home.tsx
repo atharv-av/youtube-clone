f223d451f510f58c1f1f37c4feb86f659b43c34e
atharv-av <atharvv333@gmail.com>
2023-08-20 15:41:11 +0530

    created sidebar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 function Home() {
   return (
-    <div>
-      <Navbar />
+    <div className="max-h-screen overflow-hidden">
+      <div className="h-[7.5vh]">
+        <Navbar />
+      </div>
+      <div className="flex h-[92.5vh]">
+        <Sidebar />
+      </div>
     </div>
   );
 }
